ec18bb800cf1353d338c9631f9a33c9cdae205a5
kevin120202 <kevindelapaz21@gmail.com>
2024-05-24 09:09:56 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+
+    return (
+        <div>
+            <h1>Memory Card</h1>
+        </div>
+    )
+}
+
+export default App
